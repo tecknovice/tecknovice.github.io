6133e6fefcd82198ad5ef8924db17e61e965b526
tecknovice <tecknovice@gmail.com>
2018-09-22 23:29:42 +0700

    update table project

--- table/main.js
+++ table/main.js
@@ -1,88 +1,80 @@
+function Employee(avatar, name, email, job, address, phone, age) {
+    this.avatar = avatar;
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.address = address;
+    this.phone = phone;
+}
+function generateFakeData(quantity){
+    data = [];
+    for (i = 1; i <= quantity; i++) {
+        randomAvatar = faker.image.avatar();
+        randomName = faker.name.findName();
+        randomEmail = faker.internet.email();
+        randomJob = faker.name.jobTitle();
+        randomAddress = faker.fake('{{address.zipCode}} {{address.streetName}} Street, {{address.county}} County, {{address.city}} {{address.stateAbbr}}');
+        randomPhone = faker.phone.phoneNumberFormat(2);
+        randomAge = faker.random.number({ min: 18, max: 60 });
+        e = new Employee(randomAvatar, randomName, randomEmail, randomJob, randomAddress, randomPhone, randomAge);
+        data.push(e);
+    }
+    return data;
+}
 let app = new Vue({
     el: "#app",
     data: {
-        tableName: 'My Table',
+        tableName: 'Employees',
         ascending: true,
         sortName: '',
-        products: [
-            {
-                image: '1',
-                name: 'fan',
-                code: 'F123',
-                price: '$100',
-                quantity: '10'
-            },
-            {
-                image: '2',
-                name: 'normal table',
-                code: 'T433',
-                price: '$20',
-                quantity: '30'
-            },
-            {
-                image: '3',
-                name: 'chair',
-                code: 'C200',
-                price: '$10',
-                quantity: '5'
-            },
-            {
-                image: '4',
-                name: 'fabulous table',
-                code: 'T400',
-                price: '$220',
-                quantity: '3'
-            },
-            {
-                image: '5',
-                name: 'saving table',
-                code: 'T119',
-                price: '$10',
-                quantity: '3000'
-            }
-        ]
+        rows: []
+    },
+    created() {
+        data = localStorage.getItem('Employees');
+        if(data==null){
+            this.rows = generateFakeData(50);
+            localStorage.setItem("Employees", JSON.stringify(this.rows));
+        }else{
+            this.rows = JSON.parse(data);
+        }
     },
     computed: {
-        attributes: function () {
-            if (this.products.length == 0) {
+        columns: function () {
+            if (this.rows.length == 0) {
                 return [];
             } else {
-                return Object.keys(this.products[0]);
+                return Object.keys(this.rows[0]);
             }
         }
     },
     methods: {
-        sort: function (attr) {
-            if(attr=='image'){
+        sort: function (column) {
+            if (column == 'avatar') {
                 return;
             }
-            if(this.sortName == attr){
+            if (this.sortName == column) {
                 this.ascending = !this.ascending;
-            }else{
+            } else {
                 this.ascending = true;
-                this.sortName = attr;
+                this.sortName = column;
             }
             ascending = this.ascending;
-            this.products.sort(function(i,j){
+            this.rows.sort(function (i, j) {
                 let value_i, value_j;
-                if(attr=='price'){
-                    value_i = Number(i[attr].slice(1));
-                    value_j = Number(j[attr].slice(1));
-                }else if(attr=='quantity'){
-                    value_i = Number(i[attr]);
-                    value_j = Number(j[attr]);
-                }
-                else{
-                    value_i = i[attr];
-                    value_j = j[attr];
-                }
-                if(ascending){
-                    return (value_i < value_j);
+                if (column == 'age') {
+                    value_i = Number(i[column]);
+                    value_j = Number(j[column]);
                 }else{
+                    value_i = i[column];
+                    value_j = j[column];
+                }
+                if (ascending) {
+                    return (value_i < value_j);
+                } else {
                     return (value_i > value_j);
                 }
             });
-            console.log(this.products);
+            console.log(this.rows);
         }
     }
 });
